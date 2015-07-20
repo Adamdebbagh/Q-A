83448bb434e2a134dce52c604365b2b858f23a1e
Adam debbagh <adamdebbagh@gmail.com>
2015-07-19 23:01:59 -0400

    created answers model and implemented relashionships

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ActiveRecord::Base
+  include HasGravatar
+  has_many :answers
 
-  def gravatar
-      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}.png?d=identicon"
-  end
 end
